c079a9cc14187aa36d7d55cdc15e445600083017
Nikita <dncastledn@gmail.com>
2023-08-18 15:52:32 +0300

    alpha version

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import fs from "fs";
 const app = express()
 const PORT = 3000
 
+const __dirname = path.resolve()
 const processId = process.pid;
 const content = `Номер процесса: ${processId}\n`;
 fs.writeFileSync('log.txt', content, 'utf-8');
@@ -11,8 +12,11 @@ fs.writeFileSync('log.txt', content, 'utf-8');
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
+
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(PORT)
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`listening port 3001`)
+})
